6bc9502daa0ff9729fd696924cb696460214c4ef
sirenthe <lamanrj@code.edu.az>
2023-07-25 18:35:53 +0400

    added

--- Pronia/Areas/Admin/Controllers/FeatureController.cs
+++ Pronia/Areas/Admin/Controllers/FeatureController.cs
@@ -66,31 +66,73 @@ namespace Pronia.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public async Task<IActionResult> Delete(int id)
-        {
-            var feature = await _context.Features.FirstOrDefaultAsync(x => x.Id == id);
-            if (feature is null)
-                return NotFound();
-            return View(feature);
-        }
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var feature = await _context.Features.FirstOrDefaultAsync(x => x.Id == id);
+        //    if (feature is null)
+        //        return NotFound();
+        //    return View(feature);
+        //}
 
-        [HttpPost]
-        [ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        //[HttpPost]
+        //[ActionName("Delete")]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var feature = await _context.Features.FirstOrDefaultAsync(x => x.Id == id);
+        //    if (feature is null)
+        //        return NotFound();
+        //    int featureCount = _context.Features.Count();
+        //    if (featureCount <= 1)
+        //    {
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    _context.Features.Remove(feature);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+            public async Task<IActionResult> Delete(int id)
         {
-            var feature = await _context.Features.FirstOrDefaultAsync(x => x.Id == id);
-            if (feature is null)
-                return NotFound();
-            int featureCount = _context.Features.Count();
-            if (featureCount <= 1)
+            IQueryable<Feature> query = _context.Features.AsQueryable();
+
+
+            if (await query.Features.CountAsync(f => !f.IsDeleted))
             {
-
-                return RedirectToAction(nameof(Index));
+                return BadRequest();
             }
-            _context.Features.Remove(feature);
+
+            var feature = await query.Features.FirstOrDefaultAsync(f => f.Id == id && !f.IsDeleted);
+            if (feature is null) return NotFound();
+            DeleteFeatureViewModel deleteFeatureViewModel = new DeleteFeatureViewModel
+            {
+                Image = feature.Image,
+                Title = feature.Title,
+
+                Description = feature.Description,
+
+            };
+            return View();
+        }
+        [HttpPost
+            ]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async  Task<IActionResult> DeleteFeature (int id)
+        {
+            var feature = await _context.Features.FirstOrDefaultAsync(f => f.Id == id && !f.IsDeleted);
+            if (feature is null) return NotFound();
+            feature.IsDeleted= true;
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+
+
+
         }
+
+
+
+
         public async Task<IActionResult> Update(int id)
         {
            var feature= await _context.Features.FirstOrDefaultAsync(f => f.Id == id && !f.IsDeleted);
@@ -103,6 +145,27 @@ namespace Pronia.Areas.Admin.Controllers
                 Title = feature.Title,
             };
             return View(updateFeatureViewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id , UpdateFeatureViewModel updateFeatureViewModel)
+        {
+            if(!ModelState .IsValid)
+            {
+                return View();
+                
+                var feature= await _context.Features.FirstOrDefaultAsync(f=>f.Id== id && !f.IsDeleted);
+   if(feature is null) return NotFound();
+   feature.Description = updateFeatureViewModel.Description;    
+                feature.Title = updateFeatureViewModel.Title;
+                feature.Image
+                    = updateFeatureViewModel.Image;
+                _context.Features.Update(feature);
+
+                await _context.SaveChangesAsync();
+
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Pronia/Areas/Admin/ViewModels/FeatureViewModels/DeleteFeatureViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pronia.Areas.Admin.ViewModels.FeatureViewModels
+{
+    public class DeleteFeatureViewModel
+    {
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+        public string Title { get; set; }
+    }
+}
